04f3f6c6edf0a935d07333ced5842f9fb8a46830
Maksim Gorodov <silencer.web@gmail.com>
2019-06-16 15:23:54 +0500

    Add some fixes

--- src/api/user.js
+++ src/api/user.js
@@ -4,27 +4,27 @@ const { API_URL } = require('../constants');
 
 const getUser = (userId) => {
   return axios.get(`${API_URL}/user`, { data: { id: userId } })
-    .then((response) => response.data)
+    .then(({ data: { user } }) => user)
     .catch((error) => {
-      console.log(error.data ? error.data.message : error);
+      console.log(error.response ? error.response.data.message : error);
       return null;
     });
 };
 
 const saveUser = (newUser) => {
   return axios.post(`${API_URL}/user`, newUser)
-    .then((response) => response.data)
+    .then(({ data: { user } }) => user)
     .catch((error) => {
-      console.log(error.data ? error.data.message : error);
+      console.log(error.response ? error.response.data.message : error);
       return null;
     });
 };
 
 const updateUser = (updatedUser) => {
   return axios.put(`${API_URL}/user`, updatedUser)
-    .then((response) => response.data)
+    .then(({ data: { user } }) => user)
     .catch((error) => {
-      console.log(error.data ? error.data.message : error);
+      console.log(error.response ? error.response.data.message : error);
       return null;
     });
 };
